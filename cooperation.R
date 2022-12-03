d5069bf20def19959ce347e91e9744ddaee8a398
Shashank Pritam <shashankpritam@outlook.com>
2022-12-03 00:35:11 -0600

    further updates

--- cooperation.R
+++ cooperation.R
@@ -1,22 +1,24 @@
 library('plot.matrix')
 library(gifski)
 library(tensorr)
-
+library('dplyr')
 
 #Immigrant Variable Assigned
 base_PTR = 0.12
 random_coop_defect = list(0, 1)
 random_color = list("blue", "black", "green", "yellow")
+cell_size = 50
+matrix_size = cell_size*cell_size
 
 #Storage Dataframe Initialized
 df <- data.frame(matrix(ncol = 7, nrow = 0))
 colnames(df) <- c('row', 'column', 'occupancy', 'tag1', 'tag2', 'tag3', 'PTR')
 
 #The visual Matrix Created
-Random_Matrix <- matrix(runif(2500),nrow=50)
-Game_Matrix <- matrix(0, nrow = 50, ncol = 50)
-Graph_Matrix <- matrix(nrow = 50, ncol = 50)
-dim(Graph_Matrix)<-c(50,50)
+Random_Matrix <- matrix(runif(matrix_size),nrow=cell_size)
+Game_Matrix <- matrix(0, nrow = cell_size, ncol = cell_size)
+Graph_Matrix <- matrix(nrow = cell_size, ncol = cell_size)
+dim(Graph_Matrix)<-c(cell_size, cell_size)
 
 #Transition Storage Dataframe
 tsdf <- data.frame(matrix(ncol = 21, nrow = 0))
@@ -39,8 +41,8 @@ while (i < gen)
   #print(immigrant_tag_2_same_color)
   #print(immigrant_tag_3_diff_color)   
   #Immigrant Placement in the Dataframe Begins
-  random_column = (sample((1:50), size=1, replace=TRUE))
-  random_row = (sample((1:50), size=1, replace=TRUE))
+  random_column = (sample((1:cell_size), size=1, replace=TRUE))
+  random_row = (sample((1:cell_size), size=1, replace=TRUE))
   #print(random_row)
   #print(random_column)
 
@@ -54,11 +56,17 @@ while (i < gen)
   occupancy.function <- function(x, y) {
     loc = paste(x, y, 1, sep=" ")
     if (loc %in% paste(df$row, df$column, df$occupancy, sep=" ") == TRUE){
-      print(1)
-    }else{
-      print(0)
-}}
+      return (1)
+    } else {
+      return (0)
+    }
+  }
   
+  ## Function to return tags of cell
+  tags.function <- function(x, y) {
+    loc_id = paste(x, y, 1, sep=" ")
+    return(filter(df, row == x & column == y & occupancy == 1))
+
   
   #print(df)
   
@@ -74,9 +82,9 @@ while (i < gen)
       Graph_Matrix[random_row, random_column] <- "Selfish"
     } else {
       Graph_Matrix[random_row, random_column] <- "Null"
-      print(Game_Matrix[row,col])
-      print(c(random_row, random_column))
-      print(c(row, col))
+      #print(Game_Matrix[row,col])
+      #print(c(random_row, random_column))
+      #print(c(row, col))
     }
   {name = paste('Matrix_',i,'_plot.png', sep='')}
   png(name,width=9,height=7.5,units='in',res=400)
@@ -87,13 +95,54 @@ while (i < gen)
   par(mar=c(5.1, 4.1, 4.1, 4.1),pty='s')
   plot(Graph_Matrix, col=topo.colors, main = "Graph Matrix", xlab = "Cell", ylab = "Cell",)
   
-  for(idx in 1:50)
+  
+  for(idx in 1:cell_size)
   {
-    for(idy in 1:50)
+    for(idy in 1:cell_size)
     {
-      print(c(idx, idy))
       Arow = idx
       Acolumn = idy
+      AO = occupancy.function(Arow, Acolumn)
+      if (Arow == cell_size){
+        ANSrow = 1
+        ANScolumn = Acolumn
+      } else {
+        ANSrow = Arow+1
+        ANScolumn = Acolumn}
+    
+      if (Acolumn == 1){
+        ANWrow = Arow
+        ANWcolumn = cell_size
+      } else {
+        ANWrow = Arow
+        ANWcolumn = Acolumn-1}
+       
+      if (Arow == 1){
+        ANNrow = cell_size
+        ANNcolumn = Acolumn
+      } else {
+        ANNrow = Arow-1
+        ANNcolumn = Acolumn}
+      
+      if (Acolumn == cell_size){
+        ANErow = Arow
+        ANEcolumn = 1
+      } else {
+        ANErow = Arow
+        ANEcolumn = Acolumn+1}
+        
+      print(c(Arow, Acolumn, ANEcolumn))
+      SO = occupancy.function(ANSrow, ANScolumn)
+      WO = occupancy.function(ANWrow, ANWcolumn)
+      NO = occupancy.function(ANNrow, ANNcolumn)
+      EO = occupancy.function(ANErow, ANEcolumn)
+      
+      NSO = tags.function(ANNrow, ANNcolumn)
+      if (NSO.empty == FALSE){
+      print(NSO)
+      } else {
+      return}
+        
     }
   }
 
